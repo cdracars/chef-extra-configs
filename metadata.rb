43ce71aad8eea6d25a4766ca60b42fc1b518854c
Cody Dracars <cdracars@gmail.com>
2012-08-29 10:29:56 -0500

    updates to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Dracars Designs"
+maintainer_email "cdracars@gmail.com"
 license          "All rights reserved"
-description      "Installs/Configures usao"
+description      "Installs/Configures drupal-extras"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
