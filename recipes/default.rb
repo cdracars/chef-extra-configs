3f0fc3e54bd6c8e8771eea1d4015733643c62f39
Cody Dracars <cdracars@gmail.com>
2013-04-15 11:09:22 -0500

    added key for bitbucket

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,9 @@
 # To-Do add attributes to abstract values
   # Known Hosts
   ssh_known_hosts_entry 'github.com'
-  ssh_known_hosts_entry 'bitbucket.com'
+  ssh_known_hosts_entry 'bitbucket.com' do
+    key '207.223.240.181 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAubiN81eDcafrgMeLzaFPsw2kNvEcqTKl/VqLat/MaB33pZy0y3rJZtnqwR2qOOvbwKZYKiEO1O6VqNEBxKvJJelCq0dTXWT5pbO2gDXC6h6QDXCaHo6pOHGPUy+YBaGQRGuSusMEASYiWunYN0vCAI8QaXnWMXNMdFP3jHAJH0eDsoiGnLPBlBp4TNm6rYI74nMzgz3B9IikW4WVK+dc8KZJZWYjAuORU3jc1c/NPskD2ASinf8v3xnfXeukU0sJ5N6m5E8VLjObPEO+mN2t/FZTMZLiFqPWc/ALSqnMnnhwrNi2rbfg/rd/IpL8Le3pSBne8+seeFVBoGqzHM9yXw=='
+  end
 
   # Requried to install APC.
   package "libpcre3-dev"
