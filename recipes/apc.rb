b534c2dac6711ea467ccd515595f27803f958ce1
Cody Dracars <cdracars@gmail.com>
2013-04-16 08:52:39 -0500

    updated to use package instead of pear

--- recipes/apc.rb
+++ recipes/apc.rb
@@ -8,10 +8,19 @@
 
 include_recipe "php"
 
-package "libpcre3-dev"
+#package "libpcre3-dev"
 
-php_pear "apc" do
-  directives(:shm_size => "70M")
-  version "3.1.6" #ARGH!!! debuging enabled on APC builds circa 5/2011. Pin back.
-  action :install
+#php_pear "apc" do
+#  directives(:shm_size => "70M")
+#  action :install
+#end
+
+package "php-apc"
+template "#{node['php']['ext_conf_dir']}/apc.ini" do
+  source "apc.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(:name => "apc", :extensions => ["apc.so"], :directives => {"shm_size" => "256"})
+  action action
 end
